695ae16a18404a04cb84397839f974089d8f4048
Ricardo Leite <ricardo.leitejr95@gmail.com>
2021-03-22 15:05:25 -0600

    Working on login fragment

---
+++ app/src/main/java/ui/LoginFragment.java
@@ -0,0 +1,20 @@
+package ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+public class LoginFragment extends Fragment {
+
+    public static final String ARG_TITLE = "title";
+
+    // Wire in widgets
+
+    public static LoginFragment newInstance(String title) {
+        LoginFragment fragment = new LoginFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_TITLE, title);
+        fragment.setArguments(args);
+        return fragment;
+    }
+}
