b48133c1407c3d1a8e473e0013780025e115133d
Ricardo Leite <ricardo.leitejr95@gmail.com>
2021-03-25 10:16:20 -0600

    Wired Login Fragment to main activity, form is displaying correctly

--- app/src/main/java/com/example/family_map_client/MainActivity.java
+++ app/src/main/java/com/example/family_map_client/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.family_map_client;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
 import Request.RegisterRequest;
 import Result.RegisterResult;
+import ui.LoginFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +16,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment login = fm.findFragmentById(R.id.mainContainer);
+
+
+        if (login == null) {
+            login = LoginFragment.newInstance(this);
+            fm.beginTransaction().add(R.id.mainContainer, login).commit();
+        }
+
     }
 
-    RegisterRequest reqRegister = new RegisterRequest(); // Shared jar working!
+
 }
--- app/src/main/java/ui/LoginFragment.java
+++ app/src/main/java/ui/LoginFragment.java
@@ -1,11 +1,20 @@
 package ui;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import androidx.fragment.app.Fragment;
+
+import com.example.family_map_client.R;
 
 import Request.LoginRequest;
 import Request.RegisterRequest;
@@ -15,6 +24,8 @@ public class LoginFragment extends Fragment {
     public static final String ARG_TITLE = "title";
     private LoginRequest loginRequest;
     private RegisterRequest registerRequest;
+
+    private static Context thisContext;
 
     // Private variables for form
     private EditText serverHost;
@@ -28,9 +39,14 @@ public class LoginFragment extends Fragment {
     private RadioButton maleButton;
     private RadioButton femaleButton;
 
+    // Private variable for buttons
+    private Button loginButton;
+    private Button registerButton;
+
     // Wire in widgets
 
-    public static LoginFragment newInstance(String title) {
+    public static LoginFragment newInstance(Context context) {
+        thisContext = context;
         LoginFragment fragment = new LoginFragment();
         Bundle args = new Bundle();
 //        args.putString(ARG_TITLE, title);
@@ -41,6 +57,70 @@ public class LoginFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        loginRequest = new LoginRequest();
+        registerRequest = new RegisterRequest();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_login, container, false);
+
+        registerButton = (Button) v.findViewById(R.id.RegisterButton);
+
+        registerButton.setEnabled(false);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onRegisterClick();
+            }
+        });
+
+        loginButton = (Button) v.findViewById(R.id.LoginButton);
+
+        loginButton.setEnabled(false);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onLoginClick();
+            }
+        });
+
+        // Form contents
+
+        serverHost = (EditText) v.findViewById(R.id.ServerHost);
+
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+
+        serverHost.addTextChangedListener(textWatcher);
+
+
+        return v;
+    }
+
+    private void onRegisterClick() {
+
+    }
+
+    private void onLoginClick() {
 
     }
 }
