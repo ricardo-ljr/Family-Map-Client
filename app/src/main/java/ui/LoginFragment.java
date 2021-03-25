d8a8faaf5dd228ab79390ad3535e6cb913a1dfe6
Ricardo Leite <ricardo.leitejr95@gmail.com>
2021-03-25 09:37:25 -0600

    Added private variables for login fragment

--- app/src/main/java/ui/LoginFragment.java
+++ app/src/main/java/ui/LoginFragment.java
@@ -1,12 +1,32 @@
 package ui;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.fragment.app.Fragment;
+
+import Request.LoginRequest;
+import Request.RegisterRequest;
 
 public class LoginFragment extends Fragment {
 
     public static final String ARG_TITLE = "title";
+    private LoginRequest loginRequest;
+    private RegisterRequest registerRequest;
+
+    // Private variables for form
+    private EditText serverHost;
+    private EditText serverPort;
+    private EditText username;
+    private EditText password;
+    private EditText firstName;
+    private EditText lastName;
+    private EditText email;
+    private RadioGroup gender;
+    private RadioButton maleButton;
+    private RadioButton femaleButton;
 
     // Wire in widgets
 
@@ -18,5 +38,9 @@ public class LoginFragment extends Fragment {
         return fragment;
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+    }
 }
